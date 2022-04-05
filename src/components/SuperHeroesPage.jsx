76c60676ccb66196fb3ff7f531c2289666ff3cc3
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-05 15:54:07 +0600

    error message showing with React-Query

--- src/components/SuperHeroesPage.jsx
+++ src/components/SuperHeroesPage.jsx
@@ -5,22 +5,33 @@ import "./Styles/SuperHeroesPage.css";
 export default function SuperHeroesPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:4000/superheroes").then((res) => {
-      setData(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
 
+  if (error) {
+    return <h2>{error}</h2>;
+  }
+
   return (
     <>
       <h2>Traditional Super Hero Page</h2>
-      {data.map((hero) => {
+      {data?.map((hero) => {
         return <p key={hero.id}>{hero.name}</p>;
       })}
     </>
