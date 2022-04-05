c883a4c984f793396a34129184b699cf321d039c
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-04-05 23:37:37 +0600

    getting individual hero using heroId

---
+++ src/hooks/useSuperHeroData.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHero = (heroId) =>
+  axios.get(`http://localhost:4000/superheroes/${heroId}`);
+
+export const useSuperHeroData = (heroId) => {
+  return useQuery(["super-hero", heroId], () => fetchSuperHero(heroId));
+};
